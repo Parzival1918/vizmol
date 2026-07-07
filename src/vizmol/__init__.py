da32bdb7d6606eb4d88550eea8c8a98d067ce6fd
Pedro Juan Royo <pedro.juan.royo@gmail.com>
2026-07-07 14:37:45 +0100

    change hyphen

--- src/vizmol/__init__.py
+++ src/vizmol/__init__.py
@@ -1,4 +1,4 @@
-"""vizmol – molecular and crystal visualisation powered by OVITO."""
+"""vizmol - molecular and crystal visualisation powered by OVITO."""
 
 from vizmol.core import COVALENT_RADII, MoleculeVisualizer
 
